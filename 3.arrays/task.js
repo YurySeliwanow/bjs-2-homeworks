d21064cc6ea6d38dd0b2efd26fc6a7d52c23aaaa
YurySeliwanow <131137284+YurySeliwanow@users.noreply.github.com>
2024-01-17 22:46:40 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false
+	};
+	return arr1.every((value, index) => {
+		console.log(value, arr2[index]);
+		return value === arr2[index];
+	});
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filteredUsers = users.filter(user => user.gender === gender);
+	if (filteredUsers.length === 0) {
+		return 0;
+	}
+	const totaAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+	const averageAge = totaAge / filteredUsers.length;
+	return averageAge;
 }
